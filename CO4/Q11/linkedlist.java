ae3b352a679fde039e57ba43912b68ce7a5a71a0
ABHISHEKMABHI <2002@tkmce.ac.in>
2021-08-05 11:31:32 -0500

    Q11

---
+++ CO4/Q11/linkedlist.java
@@ -0,0 +1,21 @@
+package co4;
+	import java.util.*;
+	public class linkedlist {
+	public static void main(String[] args) {
+	   LinkedList<String> list = new LinkedList<String>();      
+	        list.add("Harry");
+	        list.add("Jack");
+	        list.add("Snow");
+	        list.add("leo");
+	        list.add("Martinez");
+	        list.add("Saul");
+	        
+
+	 System.out.println("The Original linked list: " + list);
+
+	  list.clear();
+
+	  System.out.println("After removing : " + list);
+	}
+	}
+	
