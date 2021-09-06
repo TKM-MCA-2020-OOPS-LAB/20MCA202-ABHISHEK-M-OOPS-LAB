9ee2593504837a0bb937862484523ec48d5f8a34
ABHISHEKMABHI <2002@tkmce.ac.in>
2021-09-06 04:28:48 -0500

    updated

---
+++ C05/Q1/Shapes.java
@@ -0,0 +1,13 @@
+package JAVA_2;
+import java.applet.*;
+import java.awt.*;
+
+
+public class Shapes extends Applet{
+    public void paint(Graphics g) {
+        g.drawLine(20, 20, 200, 20);
+        g.drawRect(20, 60, 200, 40);
+        g.drawOval(20, 120, 150, 150);
+    }
+
+}
